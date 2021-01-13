0a17eb5da8d56e02076928b0d3bcbd5e4c7593de
ileana <ileana2512@gmail.com>
2021-01-13 17:06:27 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Please enter five numbers.");
+
+number_list = [-2.4,5.1,6.0,123.8,-19.0]
+print(number_list)
+avg = sum(number_list)/len(number_list)
+print("The average of those numbers is ", round(avg,2))
+
+
+
+
+
